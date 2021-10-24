6931726d77016b099913bb17d9ef013c52c41e99
Pritha Chattopadhyay <Pritha.Chattopadhyay@gds.ey.com>
2021-10-24 12:31:17 +0530

    updated for deploying the service to azure

--- todonotes.js
+++ todonotes.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-const port = process.argv.slice(2)[0];
+const port = process.env.PORT||3001;
 const app = express();
 const cors = require('cors');
 
